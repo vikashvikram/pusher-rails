94aa530142f5fc46fce13f7d758e3c2a037086ce
Vikash Vikram <vikashvikram@gmail.com>
2015-06-02 17:50:53 +0530

    adding canvasjs chart

--- app/controllers/real_time_controller.rb
+++ app/controllers/real_time_controller.rb
@@ -2,4 +2,8 @@ class RealTimeController < ApplicationController
   def hello_world(message = "hello world")
   	Pusher.trigger('private-test_channel', 'ping', {:message => message})
   end
+
+  def push_chart(xVal = "1", yVal = "1")
+  	Pusher.trigger('private-test_channel', 'chart', {:xVal => xVal, :yVal => yVal})
+  end
 end

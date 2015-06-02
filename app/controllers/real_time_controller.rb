41eec745be6a28013e4059c6159c5ff8c42721b9
Vikash Vikram <vikashvikram@gmail.com>
2015-06-02 18:01:48 +0530

    adding real time chart

--- app/controllers/real_time_controller.rb
+++ app/controllers/real_time_controller.rb
@@ -4,6 +4,13 @@ class RealTimeController < ApplicationController
   end
 
   def push_chart(xVal = "1", yVal = "1")
-  	Pusher.trigger('private-test_channel', 'chart', {:xVal => xVal, :yVal => yVal})
+  	xVal = 1
+  	yVal = 1
+  	10.times do
+  		Pusher.trigger('private-test_channel', 'chart', {:xVal => xVal, :yVal => yVal})
+  		xVal += 1
+  		yVal += 1
+  		sleep 1
+  	end
   end
 end
